954658e79fa2a5ea97e46d7ed37cc9c2f1a98c73
Nisan Dekel <nisandekel@walla.com>
2022-01-17 13:44:34 +0200

    init environment

---
+++ Controllers/NBA.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ranked_NBA_Players.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NBA : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<string> GetRankedPlayers(string year)
+        {
+
+        }
+
+    }
+}
